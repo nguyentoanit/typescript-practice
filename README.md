2d52b2cf6e4053de5b668afd726a996edffdf155
Toan Nguyen <nguyentoandev@gmail.com>
2019-09-20 13:42:12 +0700

    Array Destructuring and Swap Variable

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# typescript-practice
+# TypeScript Practices
 This is my practice in TypeScript
+
+Follow documents on [TypeScript's Docs](https://www.typescriptlang.org/docs/home.html)
---
+++ Variable Declarations/Destructuring/Array.ts
@@ -0,0 +1,17 @@
+console.log('-----Array Destructuring-----')
+let fruits = ['Banana', 'Apple'];
+let [fruit1, fruit2] = fruits;
+
+console.log(`
+Fruit1: ${fruit1}
+Fruit2: ${fruit2}
+`)
+
+console.log('-----Swap Variable-----')
+
+let [newFruit1, newFruit2] = [fruit1, fruit2]
+
+console.log(`
+New Fruit1: ${newFruit1}
+New Fruit2: ${newFruit2}
+`)
