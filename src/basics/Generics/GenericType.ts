e8f2c3746fe7ed7879beae0462dd356298ef3582
Toan Nguyen <nguyentoandev@gmail.com>
2020-02-06 15:39:20 +0700

    Basic: Generic Type extendable and default value

--- src/basics/Generics/GenericType.ts
+++ src/basics/Generics/GenericType.ts
@@ -3,13 +3,33 @@ function identity<T>(arg: T): T {
 }
 
 // Generics can “extends”
-class A {}
-class B extends A {}
-class C {}
+class PersonA {
+  attribute: string
+}
+class PersonB extends PersonA {}
+class PersonC {}
 
-function identity2<T extends A>(arg: T): T {
+function identity2<T extends PersonA>(arg: T): T {
   return arg;
 }
 
-identity2(new A())
-identity2(new C())
+identity2(new PersonA())
+identity2(new PersonB())
+// identity2(new PersonC()) // Argument of type 'PersonC' is not assignable to parameter of type 'PersonA'.
+
+//Generics can have a “default type value”
+interface PersonD<T = string> {
+  name: T
+}
+
+const james: PersonD = {
+  name: 'James'
+}
+
+const robin: PersonD<number> = {
+  name: 123
+}
+
+// const james: PersonD = {
+//   name: 123 // is NOT valid: The expected type comes from property 'name' which is declared here on type 'PersonD<string>'
+// }
